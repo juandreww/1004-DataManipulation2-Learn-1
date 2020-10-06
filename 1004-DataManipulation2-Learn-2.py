5b33b970f8fe106b945cec4b4f18ce3200df7cd5
Juan Andrew Sasongko <juandreww@outlook.com>
2020-10-06 22:13:47 +0700

    Data Manipulation 2

---
+++ 1004-DataManipulation2-Learn-2.py
@@ -0,0 +1,58 @@
+# Load Data as Time Series
+"""
+# Load dataset https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv', parse_dates=True, index_col='timestamp')
+gag = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv')
+# Cetak 5 data teratas
+print(gaq.head())
+# Cetak info dari dataframe gaq
+print('info')
+print(gaq.info())
+print("\n")
+print(gag.info())
+"""
+
+# Convert to Datetime
+"""
+# Load dataset https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv')
+# Cetak 5 data teratas
+print('Sebelum diubah dalam format datetime:\n', gaq.head())
+# Ubah menjadi datetime
+gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
+gaq = gaq.set_index('timestamp')
+# Cetak 5 data teratas
+print('Sesudah diubah dalam format datetime:\n', gaq.head())
+"""
+
+# Downsampling
+"""
+# Load dataset https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv')
+gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
+gaq = gaq.set_index('timestamp')
+print('Dataset sebelum di-downsampling (5 teratas):\n', gaq.head())
+
+# [1] Downsampling dari daily to weekly dan kita hitung maksimum untuk seminggu
+gaq_weekly = gaq.resample('W').mean()
+print('Downsampling daily to weekly - max (5 teratas):\n', gaq_weekly.head())
+# [2] Downsampling dari daily to quaterly dan kita hitung minimumnya untuk tiap quarter
+gaq_quaterly = gaq.resample('Q').median()
+print('Downsampling daily to quaterly - min (5 teratas):\n', gaq_quaterly.head())
+"""
+
+# Upsampling
+""""""
+# Load dataset https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv
+gaq = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/LO4/global_air_quality_4000rows.csv')
+gaq['timestamp'] = pd.to_datetime(gaq['timestamp'])
+gaq = gaq.set_index('timestamp')
+print('Dataset sebelum di-upsampling (5 teratas):\n', gaq.head())
+# Upsampling dari daily to hourly dan kita hitung reratanya
+gaq_hourly = gaq.resample('H').mean()
+print('Upsampling daily to hourly - mean (5 teratas):\n', gaq_hourly.head())
+print("\n")
+
+# Resample dari daily to 2 monthly, hitung reratanya, dan fillna = 'bfill'
+gaq_2monthly = gaq.resample('2M').mean().fillna(method='bfill')
+print('Resampling daily to 2 monthly - mean - ffill (5 teratas):\n', gaq_2monthly.head())
